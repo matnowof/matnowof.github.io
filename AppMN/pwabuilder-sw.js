7349e86625d841cc35e7c1a7be28534d1ed7fdfa
matnowof <90075447+matnowof@users.noreply.github.com>
2023-02-07 14:53:07 -0300

    Update pwabuilder-sw.js

--- AppMN/pwabuilder-sw.js
+++ AppMN/pwabuilder-sw.js
@@ -1,6 +1,6 @@
 // This is the "Offline copy of pages" service worker
 
-const CACHE = "pwabuilder-offline";
+const CACHE = "pwabuilderoffline";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
@@ -16,3 +16,21 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+this.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['v2'];
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(pwabuilderoffline) {
+      return Promise.all(
+        cacheNames.filter(function(pwabuilderoffline) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(pwabuilderoffline) {
+          return caches.delete(pwabuilderoffline);
+        })
+      );
+    })
+  );
+});
